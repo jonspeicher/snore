b0342d82cb49867325bcf97d6ee9a6c86adb84bc
Jon Speicher <jon.speicher@gmail.com>
2010-01-30 03:36:14 -0500

    Replace generic snore.py with plugin.py
    Add a stub plugin class, rename snore.py to plugin.py to help better identify what it contains.

---
+++ snore/plugin.py
@@ -0,0 +1,14 @@
+# snore
+# Copyright (c) 2010 Jonathan Speicher
+# jon.speicher@gmail.com
+# Licensed under the MIT license: http://creativecommons.org/licenses/MIT
+
+from nose.plugins import Plugin
+
+"""
+This class defines a Snarl plugin for the nose test runner.  It will provide notification of test
+events via the Snarl system notification agent when nose is run.
+"""
+
+class SnorePlugin(Plugin):
+    pass
--- snore/snore.py
+++
@@ -1,4 +0,0 @@
-# snore
-# Copyright (c) 2010 Jonathan Speicher
-# jon.speicher@gmail.com
-# Licensed under the MIT license: http://creativecommons.org/licenses/MIT
