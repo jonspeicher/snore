b6b8aa1abca40499372275a918e4da039b0ceb44
Jon Speicher <jon.speicher@gmail.com>
2010-01-30 03:38:49 -0500

    Add first-draft setup.py
    After messing around with setuptools, add a setup.py that packages snore as an egg and installs it into site-packages.  An entry_point hooks the code into nose as a plugin.  The code is deployed as a Python package and the entry_point references the package/module/class name.

---
+++ setup.py
@@ -0,0 +1,29 @@
+# snore
+# Copyright (c) 2010 Jonathan Speicher
+# jon.speicher@gmail.com
+# Licensed under the MIT license: http://creativecommons.org/licenses/MIT
+
+"""
+This script installs snore as a nose plugin.
+"""
+
+try:
+    import ez_setup
+    ez_setup.use_setuptools()
+except ImportError:
+    pass
+
+from setuptools import setup
+
+setup(
+    name = 'snore',
+    version = '0.1',
+    author = 'Jon Speicher',
+    author_email = 'jon.speicher@gmail.com',
+    description = 'A Snarl plugin for nose',
+    url = "http://github.com/jonspeicher/snore",
+    license = 'MIT',
+    install_requires = ['nose>=0.11.1'],
+    packages = ['snore'],
+    entry_points = { 'nose.plugins': ['snore = snore.plugin:SnorePlugin'] }
+    )
