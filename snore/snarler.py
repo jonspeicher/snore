2e3aa4965937da12368672a4704fa3f316e79c7a
Jon Speicher <jon.speicher@gmail.com>
2010-02-12 18:13:50 -0500

    Make the Snarler snarl
    Remove debug logging code from the Snarler class and replace it with a dependency on PySnarl and actual code to display the title, body, and icon.

--- snore/snarler.py
+++ snore/snarler.py
@@ -4,16 +4,10 @@ to display status and reports via Snarl in an uncomplicated fashion."""
 # Copyright (c) 2010 Jonathan Speicher (jon.speicher@gmail.com)
 # Licensed under the MIT license: http://creativecommons.org/licenses/MIT
 
-import logging
+import PySnarl
 
 class Snarler(object):
     """Interface with Snarl to display status and reports"""
-    
-    def __init__(self):
-        self._logger = logging.getLogger('nose.plugins.snore')
         
     def snarl(self, title, body, icon):
-        self._logger.debug('snore ----------------------------------')
-        self._logger.debug(icon)
-        self._logger.debug(title)
-        self._logger.debug(body)
+        PySnarl.snShowMessage(title, body, timeout = 5, iconPath = icon)
